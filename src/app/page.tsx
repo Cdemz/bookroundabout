2f7f5e5f8da4501b8c4fcea4562eba9b9e5e8123
Ademola <gbengaadepoju75@gmail.com>
2023-08-29 18:00:24 -0500

    general

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
 "use client";
 import Herosection from "./components/Herosection";
 import ShopCat from "./components/ShopCat";
-import BestSeller from "./components/BestSeller";
-import Product from "./components/Product";
-import data from "./BooksData.json";
-// import { Menu, Transition } from "@headlessui/react";
-import { FaFilter } from "react-icons/fa";
-// import MenuItem from "./components/MenuItem";
-import { Fragment } from "react";
-import { RecoilRoot } from "recoil";
-import { Toaster } from "react-hot-toast";
 import AdvancedProduct from "./components/AdvancedProduct";
-import SubProduct from "./components/SubProduct";
 
 export default function Home() {
-  // const fictionLinks = [
-  //   "Ficition",
-  //   "Children Books",
-  //   "Non-Ficition",
-  //   "Classics",
-  //   "Romance",
-  //   "Crime & Thriller",
-  //   "Fantasy & Horror",
-  //   "Poetry & Drama",
-  // ].map((linkText) => {
-  //   return <MenuItem linkText={linkText} />;
-  // });
   return (
     <main>
       <Herosection />
