5aad32db197cf8fdb7cc4d869d8796a06912c55d
Ademola <gbengaadepoju75@gmail.com>
2023-09-10 17:56:12 -0500

    api down

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,64 +1,78 @@
 import type { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { GithubProfile } from "next-auth/providers/github";
-import Cookies from "js-cookie";
 
 export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
-      profile(profile: GithubProfile) {
-        console.log(profile);
-        return {
-          ...profile,
-          role: profile.role ?? "user",
-          id: profile.id.toString(),
-          image: profile.avatar_url,
-        };
-      },
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
     CredentialsProvider({
-      name: "Credentials",
       credentials: {
-        username: { label: "Username", type: "text" },
+        email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        const res = await fetch("http://booksra.helioho.st/v1/user/login", {
-          method: "POST",
-          body: JSON.stringify(credentials),
-          headers: { "Content-Type": "application/json" },
-        });
-        const data = await res.json();
-        if (data.status === "ok") {
-          return data.user;
+      authorize: async (credentials) => {
+        try {
+          const res = await fetch("http://booksra.helioho.st/v1/user/login", {
+            method: "POST",
+            body: JSON.stringify(credentials),
+            headers: { "Content-Type": "application/json" },
+          });
+
+          if (!res.ok) {
+            // Handle non-success HTTP status codes (e.g., 401 Unauthorized)
+            console.error("API Error: Status Code", res.status);
+            return null; // Return null for authentication failure
+          }
+
+          const data = await res.json();
+          console.log("API Response:", data);
+
+          if (data.token && data.user) {
+            // Token successfully retrieved from the API
+            return {
+              id: data.user.id.toString(),
+              email: data.user.email,
+              role: data.user.role || "user", // Provide a default role if not available
+              // Add other user properties if available in the API response
+            };
+          } else {
+            // Token not found in the API response
+            console.error("Authentication failed: Token not found");
+            return null; // Return null for other API response statuses
+          }
+        } catch (error) {
+          // Handle other errors, such as network issues
+          console.error("API Error:", error);
+          return null; // Return null for errors
         }
-        return null;
       },
     }),
   ],
   callbacks: {
-    async jwt({ token, user, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-        token.user = user;
+    async jwt({ token }) {
+      // JWT callback: This is where you modify the token before it's saved
+      if (token) {
+        // Modify the token if needed, e.g., decode and assign values
+        const { id, email, role } = token;
+        token.id = id;
+        token.email = email;
+        token.role = role;
       }
-      console.log(user);
       return token;
     },
-    async session({ session, token, user }) {
-      session.accessToken = token.accessToken;
-      session.user = token.user;
+    async session({ session, token }) {
+      // Session callback: This is where you populate the session object
+      session.user = token;
       return session;
     },
   },
   pages: {
     signIn: "/login", // Use the custom sign-in page
-    //   // signOut: "/signout",
-    //   // error: "/error",
-    //   // verifyRequest: "/verify-request",
     newUser: "/register", // Do not use a custom new user creation page
   },
 };
+
+export default options;
