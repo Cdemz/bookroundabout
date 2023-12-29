9c285bae3fcf2931767d6947d0414b0ca9cb0f9b
Ademola <gbengaadepoju75@gmail.com>
2023-12-29 17:46:20 -0600

    i have to push this first

--- src/app/PaymentVerification/page.tsx
+++ src/app/PaymentVerification/page.tsx
@@ -3,9 +3,13 @@
 import React, { useEffect } from "react";
 import { API_BASE_URL } from "../utils/api"; // Adjust the path as per your project structure
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { BeatLoader } from "react-spinners";
 
 const PaymentVerification = () => {
+  const router = useRouter();
   // Function to extract code from URL and verify payment
+
   const verifyPayment = async () => {
     const queryParams = new URLSearchParams(window.location.search);
     const code = queryParams.get("code");
@@ -21,16 +25,23 @@ const PaymentVerification = () => {
         );
         const result = await response.json();
 
-        // Handle the verification result
-        // For example, display a success message
-        toast.success("Payment Verified Successfully!");
+        // Assuming you check the result to determine success or failure
+        if (result.success) {
+          toast.success("Payment Verified Successfully!");
+          router.push("/success"); // Redirect to success page
+        } else {
+          toast.error("Payment verification failed.");
+          router.push("/failure"); // Redirect to failure page
+        }
       } catch (error) {
-        console.error("Error verifying purchase:", error);
+        // console.error("Error verifying purchase:", error);
         toast.error("Error verifying payment.");
+        router.push("/failure"); // Redirect to failure page
       }
     } else {
-      console.error("Code parameter is missing in the URL");
+      //   console.error("Code parameter is missing in the URL");
       toast.error("Code parameter is missing.");
+      router.push("/failure"); // Redirect to failure page if code is missing
     }
   };
 
@@ -41,7 +52,11 @@ const PaymentVerification = () => {
   return (
     <div>
       {/* You can put some loading indicator or message here */}
-      Verifying Payment...
+
+      <div className="w-full flex flex-col gap-6 items-center justify-center py-20 text-[var(--color-text)] font-bold">
+        <p>Verifying Payment...</p>
+        <BeatLoader color="#4d5bf8" size={40} />
+      </div>
     </div>
   );
 };
